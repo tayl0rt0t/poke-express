571a4d22ebe1edd20d8abae44f68dff8245b2028
Taylor Treadwell <taylortread91@gmail.com>
2023-09-09 13:35:28 -0400

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/',(req,res)=> {
     res.send('Welcome to the Pokemon App!')
 })
 app.get('/pokemon',(req,res) => {
-    res.send(pokemon)
+    res.render('Index')
 })
 
 
---
+++ views/Index.jsx
@@ -0,0 +1,18 @@
+const React = require('react');
+
+const indStyle = {
+    color: '#FFFF66',
+    backgroundColor:'#1919FF',
+    
+}
+
+function Index(){
+    
+    return(
+    <div style={indStyle}>
+        <h1>See all the Pokemon!</h1>
+
+    </div>
+    )
+}
+module.exports = Index;
